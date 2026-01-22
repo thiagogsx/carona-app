01be906246c558e9b7c9c90252b94596faf378b3
thiagogsx <thiagomunizalves@gmail.com>
2026-01-22 10:26:17 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,20 @@
-const CACHE_NAME = 'carona-github-v11'; // V11 - DESTRAVAR E ATUALIZAR
-const urlsToCache = ['./', './index.html', './manifest.json'];
-self.addEventListener('install', event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))); });
-self.addEventListener('fetch', event => { event.respondWith(fetch(event.request).catch(() => caches.match(event.request))); });
+const CACHE_NAME = 'carona-github-v12'; // V12 - VOLTANDO AO NORMAL
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
